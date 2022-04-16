b9841f992b62e8bf9c95235108101ae9c0d3bea7
Geovani Paz <geovanipaz7@gmail.com>
2022-04-15 23:29:44 -0300

    implementação do model

--- redesocial/core/models.py
+++ redesocial/core/models.py
@@ -12,4 +12,32 @@ class Profile(models.Model):
     def __str__(self) -> str:
         return f'{self.user.username} Profile'
     
-
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    post_text = models.CharField(max_length=2000)
+    post_picture = models.FileField(default='default.jpg',
+                                    upload_to='post_picture')
+    
+    def __str__(self) -> str:
+        return self.user.username
+    
+#armazena pessoas que seguiu   
+class Following(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    following_user = models.CharField(max_length=100, null=True)
+    
+    def __str__(self) -> str:
+        return self.following_user.username
+    
+class Follower(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    follower_user = models.CharField(max_length=100, null=True)
+    
+    def __str__(self) -> str:
+        return self.follower_user.username
+    
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_text = models.CharField(default="", max_length=2000)
